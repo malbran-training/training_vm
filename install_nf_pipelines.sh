f9cec6e02a2a1827543a670b58b941429fe83487
Jacqui Keane <872885+jacquikeane@users.noreply.github.com>
2022-11-02 01:48:56 +0000

    Update install_nf_pipelines.sh

--- install_nf_pipelines.sh
+++ install_nf_pipelines.sh
@@ -31,7 +31,7 @@ mkdir nf-pipelines
 cd nf-pipelines
 
 echo "Installing pipelines from nf core..."
-nf-core download --compress none --container singularity --revision 1.5 fetchngs
+#nf-core download --compress none --container singularity --revision 1.5 fetchngs
 nf-core download --compress none --container singularity --revision 1.0.0 bactmap
 
 cd ..
@@ -40,7 +40,7 @@ cd ..
 echo "Installing wrapper scripts for nextflow pipelines..."
 git clone https://github.com/pgimer-training/nf_pipeline_scripts
 cd nf_pipeline_scripts
-cp run_fetch*.sh $MINICONDA/envs/nextflow-pipelines/bin
+#cp run_fetch*.sh $MINICONDA/envs/nextflow-pipelines/bin
 cp run_bactmap*.sh $MINICONDA/envs/nextflow-pipelines/bin
 
 conda deactivate
