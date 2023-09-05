009e8a8efee563fd28a207cffc4efcda0d70300c
Jacqui Keane <872885+jacquikeane@users.noreply.github.com>
2023-09-05 08:59:39 +0100

    Create install_vcf2pseudogenome.sh

---
+++ install_vcf2pseudogenome.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Bespoke recipie to install vcf2pseudogenomes.sh via conda
+# Author: Jacqui Keane
+#
+# Usage: install_vcf2pseudogenomes.sh
+#
+
+set -x
+set -eu
+
+export MINICONDA="$HOME/miniconda"
+export MINICONDA_BIN_LOCATION="$MINICONDA/bin"
+export PATH="$MINICONDA_BIN_LOCATION:$PATH"
+
+source $MINICONDA/etc/profile.d/conda.sh
+
+# Take a copy of the software 
+cd ~/src
+wget https://raw.githubusercontent.com/nf-core/bactmap/master/bin/vcf2pseudogenome.py
+
+# Put the script in the bin directory
+mv vcf2pseudogenome.py $MINICONDA_BIN_LOCATION
+
+set +eu
+set +x
